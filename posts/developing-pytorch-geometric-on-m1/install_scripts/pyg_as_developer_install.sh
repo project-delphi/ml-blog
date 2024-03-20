d4ae4fb6dfc7b1d5c2f337131bc37048ef040c92
Ravi Kalia <ravkalia@gmail.com>
2024-03-20 12:08:44 -0500

    chore: remove unused scripts
    These scripts have been moved to GitHub gists and links have been added in the blog notebook.

--- posts/developing-pytorch-geometric-on-m1/install_scripts/pyg_as_developer_install.sh
+++ posts/developing-pytorch-geometric-on-m1/install_scripts/pyg_as_developer_install.sh
@@ -1,53 +1,56 @@
-#!/bin/bash
-# This script sets up a Python virtual environment and installs PyTorch and PyTorch Geometric.
-
-# Set variables
-DIR="$HOME/Code/throwaway/pytorch-geometric-user-install"
+# set variables here
+DIR="$HOME/Code/throwaway/pytorch-geometric-developer-install"
+PYTHON_VERSION=3.11
 RECENT_TORCH_VERSION=2.2.0
-YOUR_USERNAME="project-delphi"
+GITHUB_USERNAME="project-delphi"
+MIN_MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion)
 
-# Create the directory and navigate into it
-mkdir -p "$DIR"
-cd "$DIR"
-
-# Create a Python virtual environment and activate it
-python3 -m venv .venv
-source .venv/bin/activate
-
-# Upgrade pip
-pip install -q --upgrade pip
-
-# Install dependencies for torch
-pip install -q numpy  # to remove user warning with torch install
-pip install -q mpmath==1.3.0  # bugfix
-
-# Install Xcode if not already installed
-xcode-select --install  > /dev/null 2>&1
-
-# Install Miniconda for Apple Silicon if not already installed
-if [ -d "$HOME/anaconda3" ] || [ -d "$HOME/miniconda3" ]
+# install miniconda for apple silicon, if not already installed
+if [ ! -d "$HOME/anaconda3" ] && [ ! -d "$HOME/miniconda3" ]
 then
-    echo "Conda is installed"
-else
+    echo "installing conda..."
     curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
-    sh Miniconda3-latest-MacOSX-arm64.sh -b
+    sh Miniconda3-latest-MacOSX-arm64.sh -b -u  > /dev/null 2>&1
 fi
 
-# Install torch
+mkdir -p "$DIR"
+cd "$DIR"
+conda create --yes  -p $DIR/.venv python=$PYTHON_VERSION  > /dev/null 2>&1
+eval "$(conda shell.bash hook)"
+conda activate $DIR/.venv
+
+pip install -q --upgrade pip
+
+##### TORCH BUILD AND INSTALL ON M1, to use GPUs #####
+pip install -q numpy # to remove user warning with torch install
+pip install -q mpmath==1.3.0 # bugfix
+xcode-select --install  > /dev/null 2>&1 # if xcode not installed
+
+###### install pytorch ######
 pip install -q --pre torch==$RECENT_TORCH_VERSION torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 
-# Uninstall torch-geometric if it's already installed
-pip uninstall -y torch-geometric
+# install dev build dependencies
+pip install -q cmake
+pip install -q ninja wheel
+pip install -q git+https://github.com/pyg-team/pyg-lib.git
+MACOSX_DEPLOYMENT_TARGET=$MIN_MACOSX_DEPLOYMENT_TARGET CC=clang CXX=clang++ python -m pip -q --no-cache-dir  install  torch-scatter
+MACOSX_DEPLOYMENT_TARGET=$MIN_MACOSX_DEPLOYMENT_TARGET CC=clang CXX=clang++ python -m pip -q --no-cache-dir  install  torch-sparse
+MACOSX_DEPLOYMENT_TARGET=$MIN_MACOSX_DEPLOYMENT_TARGET CC=clang CXX=clang++ python -m pip -q --no-cache-dir  install  torch-cluster
+MACOSX_DEPLOYMENT_TARGET=$MIN_MACOSX_DEPLOYMENT_TARGET CC=clang CXX=clang++ python -m pip -q --no-cache-dir  install  torch-spline-conv
 
-# Clone the PyTorch Geometric repository and install it
-git clone "https://github.com/$YOUR_USERNAME/pytorch_geometric"
+# clone the forked repository and rebase to original
+git clone "https://github.com/$GITHUB_USERNAME/pytorch_geometric.git"  2>/dev/null
 cd pytorch_geometric
-pip install -e ".[dev,full]"
+if ! git remote | grep -q 'upstream'; then
+    git remote add upstream "https://github.com/pyg-team/pytorch_geometric"
+fi
+git fetch upstream  -q
+git rebase upstream/master
 
-# Check versions
+# build dev install
+MACOSX_DEPLOYMENT_TARGET=$MIN_MACOSX_DEPLOYMENT_TARGET CC=clang CXX=clang++ python -m pip install -q --no-cache-dir -e ".[dev,full]"  #> /dev/null 2>&1
+
+# check
 python --version
 python -c "import torch; print(f'torch version: {torch.__version__}')"
 python -c "import torch_geometric as pyg; print(f'torch geometric version: {pyg.__version__}')"
-
-# Run tests
-pytest
